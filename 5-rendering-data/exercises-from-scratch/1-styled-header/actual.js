d3f389033ffdbde393f8faab03ab41b3ed2bc8ef
HYF BE <damaksinaarseniya@gmail.com>
2023-08-12 19:59:35 +0200

    Finished including 5-rendering-data

--- 5-rendering-data/exercises-from-scratch/1-styled-header/actual.js
+++ 5-rendering-data/exercises-from-scratch/1-styled-header/actual.js
@@ -1,3 +1,9 @@
 'use strict';
 
-const actual = (level, text, styling) => {};
+const actual = (level, text, styling) => {
+  const headerEl = document.createElement(`h${level}`);
+  headerEl.innerText = text;
+  headerEl.className = styling;
+
+  return headerEl;
+};
--- 5-rendering-data/exercises-from-scratch/3-quiz-form/actual.js
+++ 5-rendering-data/exercises-from-scratch/3-quiz-form/actual.js
@@ -1,3 +1,37 @@
 'use strict';
 
-const actual = ({ question, answers, correct, selected }) => {};
+const actual = ({ question, answers, correct, selected }) => {
+  const formEl = document.createElement('form');
+  const pEl = document.createElement('p');
+  pEl.innerText = question;
+  formEl.appendChild(pEl);
+
+  answers.forEach((answer, index) => {
+    const inputEl = document.createElement('input');
+    inputEl.type = 'radio';
+    inputEl.name = 'answers';
+    if (index === selected) {
+        inputEl.checked = true;
+    }
+
+    const textEl = document.createElement('text');
+    textEl.innerText = answer;
+    if (inputEl.checked) {
+        if (index === correct) {
+            textEl.className = 'correct';
+            textEl.innerText = answer + ' √';
+        } else {
+            textEl.className = 'incorrect';
+            textEl.innerText = answer + ' ✖';
+        }
+    }
+
+    const brEl = document.createElement('br');
+    formEl.append(inputEl, textEl, brEl);
+});
+
+return formEl;
+
+}
+
+  
